8e5bbd0e68a48b8a5fe00c3949d3c65ef6577e84
Carlos_Lopes <carloslopesufpa@gmail.com>
2017-10-11 16:43:12 -0300

    Versão 3
    Código Funcional. Falta melhorar a estrutura da interface do Servidor (Repetição).

--- NS3/Huenstein_v3.cc
+++ NS3/Huenstein_v3.cc
@@ -65,7 +65,7 @@ int main (int argc, char *argv[])
   uint32_t payloadSize = 1472;//bytes
   uint64_t simulationTime = 10; //seconds
   uint32_t nSta = 4;
-  uint32_t nAp = 4;
+  uint32_t nAp = 8;
 
   NodeContainer wifiStaNodes;
   wifiStaNodes.Create (nSta);
@@ -82,7 +82,7 @@ int main (int argc, char *argv[])
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("HtMcs7"), "ControlMode", StringValue ("HtMcs0"));
   WifiMacHelper mac;
 
-  NetDeviceContainer staDeviceA, staDeviceB, staDeviceC, staDeviceD, apDeviceA, apDeviceB, apDeviceC, apDeviceD;
+  NetDeviceContainer staDeviceA, staDeviceB, staDeviceC, staDeviceD, apDeviceA, apDeviceAa, apDeviceB, apDeviceBb, apDeviceC, apDeviceCc, apDeviceD, apDeviceDd;
   Ssid ssid;
 
   //Network A
@@ -96,6 +96,7 @@ int main (int argc, char *argv[])
                "Ssid", SsidValue (ssid),
                "EnableBeaconJitter", BooleanValue (false));
   apDeviceA = wifi.Install (phy, mac, wifiApNodes.Get (0));
+  apDeviceAa = wifi.Install (phy, mac, wifiApNodes.Get (1));
 
   //Network B
   ssid = Ssid ("network-B");
@@ -109,7 +110,8 @@ int main (int argc, char *argv[])
   mac.SetType ("ns3::ApWifiMac",
                "Ssid", SsidValue (ssid),
                "EnableBeaconJitter", BooleanValue (false));
-  apDeviceB = wifi.Install (phy, mac, wifiApNodes.Get (1));
+  apDeviceB = wifi.Install (phy, mac, wifiApNodes.Get (2));
+  apDeviceBb = wifi.Install (phy, mac, wifiApNodes.Get (3));
 
   //Network C
   ssid = Ssid ("network-C");
@@ -124,7 +126,8 @@ int main (int argc, char *argv[])
   mac.SetType ("ns3::ApWifiMac",
                "Ssid", SsidValue (ssid),
                "EnableBeaconJitter", BooleanValue (false));
-  apDeviceC = wifi.Install (phy, mac, wifiApNodes.Get (2));
+  apDeviceC = wifi.Install (phy, mac, wifiApNodes.Get (4));
+  apDeviceCc = wifi.Install (phy, mac, wifiApNodes.Get (5));
 
   //Network D
   ssid = Ssid ("network-D");
@@ -139,15 +142,16 @@ int main (int argc, char *argv[])
   mac.SetType ("ns3::ApWifiMac",
                "Ssid", SsidValue (ssid),
                "EnableBeaconJitter", BooleanValue (false));
-  apDeviceD = wifi.Install (phy, mac, wifiApNodes.Get (3));
+  apDeviceD = wifi.Install (phy, mac, wifiApNodes.Get (6));
+  apDeviceDd = wifi.Install (phy, mac, wifiApNodes.Get (7));
 
   /* Setting mobility model */
   MobilityHelper mobilitySta;
 
   mobilitySta.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (5.0),
+                                 "MinX", DoubleValue (7.5),
                                  "MinY", DoubleValue (5.0),
-                                 "DeltaX", DoubleValue (5.0),
+                                 "DeltaX", DoubleValue (10.0),
                                  "DeltaY", DoubleValue (10.0),
                                  "GridWidth", UintegerValue (4),
                                  "LayoutType", StringValue ("RowFirst"));
@@ -157,11 +161,11 @@ int main (int argc, char *argv[])
 
   MobilityHelper mobilityAp;  
   mobilityAp.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (7.0),
-                                 "MinY", DoubleValue (15.0),
+                                 "MinX", DoubleValue (5.0),
+                                 "MinY", DoubleValue (10.0),
                                  "DeltaX", DoubleValue (5.0),
                                  "DeltaY", DoubleValue (10.0),
-                                 "GridWidth", UintegerValue (4),
+                                 "GridWidth", UintegerValue (8),
                                  "LayoutType", StringValue ("RowFirst"));
 
   mobilityAp.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
@@ -174,87 +178,134 @@ int main (int argc, char *argv[])
   stack.Install (wifiStaNodes);
 
   Ipv4AddressHelper address;
+
   address.SetBase ("192.168.1.0", "255.255.255.0");
   Ipv4InterfaceContainer StaInterfaceA;
   StaInterfaceA = address.Assign (staDeviceA);
+
   Ipv4InterfaceContainer ApInterfaceA;
   ApInterfaceA = address.Assign (apDeviceA);
+  Ipv4InterfaceContainer ApInterfaceAa;
+  ApInterfaceAa = address.Assign (apDeviceAa);
 
   address.SetBase ("192.168.2.0", "255.255.255.0");
   Ipv4InterfaceContainer StaInterfaceB;
   StaInterfaceB = address.Assign (staDeviceB);
+
   Ipv4InterfaceContainer ApInterfaceB;
   ApInterfaceB = address.Assign (apDeviceB);
+  Ipv4InterfaceContainer ApInterfaceBb;
+  ApInterfaceBb = address.Assign (apDeviceBb);
 
   address.SetBase ("192.168.3.0", "255.255.255.0");
   Ipv4InterfaceContainer StaInterfaceC;
   StaInterfaceC = address.Assign (staDeviceC);
+  
   Ipv4InterfaceContainer ApInterfaceC;
   ApInterfaceC = address.Assign (apDeviceC);
+  Ipv4InterfaceContainer ApInterfaceCc;
+  ApInterfaceCc = address.Assign (apDeviceCc);
 
   address.SetBase ("192.168.4.0", "255.255.255.0");
   Ipv4InterfaceContainer StaInterfaceD;
   StaInterfaceD = address.Assign (staDeviceD);
+  
   Ipv4InterfaceContainer ApInterfaceD;
   ApInterfaceD = address.Assign (apDeviceD);
+  Ipv4InterfaceContainer ApInterfaceDd;
+  ApInterfaceDd = address.Assign (apDeviceDd);
 
   /* Setting applications */
   uint16_t port = 9;
+  
+
   UdpServerHelper serverA (port);
   ApplicationContainer serverAppA = serverA.Install (wifiStaNodes.Get (0));
   serverAppA.Start (Seconds (0.0));
-  serverAppA.Stop (Seconds (simulationTime + 1));
+  serverAppA.Stop (Seconds (simulationTime - 1));
 
   UdpClientHelper clientA (StaInterfaceA.GetAddress (0), port);
   clientA.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
   clientA.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
   clientA.SetAttribute ("PacketSize", UintegerValue (payloadSize));
-
   ApplicationContainer clientAppA = clientA.Install (wifiApNodes.Get (0));
   clientAppA.Start (Seconds (1.0));
-  clientAppA.Stop (Seconds (simulationTime + 1));
+  clientAppA.Stop (Seconds (simulationTime - 1));
+
+  UdpClientHelper clientAa (StaInterfaceA.GetAddress (0), port);
+  clientAa.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
+  clientAa.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
+  clientAa.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  ApplicationContainer clientAppAa = clientAa.Install (wifiApNodes.Get (1));
+  clientAppAa.Start (Seconds (1.0));
+  clientAppAa.Stop (Seconds (simulationTime - 1));
+
 
   UdpServerHelper serverB (port);
   ApplicationContainer serverAppB = serverB.Install (wifiStaNodes.Get (1));
   serverAppB.Start (Seconds (0.0));
-  serverAppB.Stop (Seconds (simulationTime + 1));
-
+  serverAppB.Stop (Seconds (simulationTime - 1));
+  
   UdpClientHelper clientB (StaInterfaceB.GetAddress (0), port);
   clientB.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
   clientB.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
   clientB.SetAttribute ("PacketSize", UintegerValue (payloadSize));
-
-  ApplicationContainer clientAppB = clientB.Install (wifiApNodes.Get (1));
+  ApplicationContainer clientAppB = clientB.Install (wifiApNodes.Get (2));
   clientAppB.Start (Seconds (1.0));
-  clientAppB.Stop (Seconds (simulationTime + 1));
+  clientAppB.Stop (Seconds (simulationTime - 1));
+
+  UdpClientHelper clientBb (StaInterfaceB.GetAddress (0), port);
+  clientBb.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
+  clientBb.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
+  clientBb.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  ApplicationContainer clientAppBb = clientBb.Install (wifiApNodes.Get (3));
+  clientAppBb.Start (Seconds (1.0));
+  clientAppBb.Stop (Seconds (simulationTime - 1));
+
 
   UdpServerHelper serverC (port);
   ApplicationContainer serverAppC = serverC.Install (wifiStaNodes.Get (2));
   serverAppC.Start (Seconds (0.0));
-  serverAppC.Stop (Seconds (simulationTime + 1));
+  serverAppC.Stop (Seconds (simulationTime - 1));
 
   UdpClientHelper clientC (StaInterfaceC.GetAddress (0), port);
   clientC.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
   clientC.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
   clientC.SetAttribute ("PacketSize", UintegerValue (payloadSize));
-
-  ApplicationContainer clientAppC = clientC.Install (wifiApNodes.Get (2));
+  ApplicationContainer clientAppC = clientC.Install (wifiApNodes.Get (4));
   clientAppC.Start (Seconds (1.0));
-  clientAppC.Stop (Seconds (simulationTime + 1));
+  clientAppC.Stop (Seconds (simulationTime - 1));
+  
+  UdpClientHelper clientCc (StaInterfaceC.GetAddress (0), port);
+  clientCc.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
+  clientCc.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
+  clientCc.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  ApplicationContainer clientAppCc = clientCc.Install (wifiApNodes.Get (5));
+  clientAppCc.Start (Seconds (1.0));
+  clientAppCc.Stop (Seconds (simulationTime - 1));
+
 
   UdpServerHelper serverD (port);
   ApplicationContainer serverAppD = serverD.Install (wifiStaNodes.Get (3));
   serverAppD.Start (Seconds (0.0));
-  serverAppD.Stop (Seconds (simulationTime + 1));
-
+  serverAppD.Stop (Seconds (simulationTime - 1));
+  
   UdpClientHelper clientD (StaInterfaceD.GetAddress (0), port);
   clientD.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
   clientD.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
   clientD.SetAttribute ("PacketSize", UintegerValue (payloadSize));
-
-  ApplicationContainer clientAppD = clientD.Install (wifiApNodes.Get (3));
+  ApplicationContainer clientAppD = clientD.Install (wifiApNodes.Get (6));
   clientAppD.Start (Seconds (1.0));
-  clientAppD.Stop (Seconds (simulationTime + 1));
+  clientAppD.Stop (Seconds (simulationTime - 1));
+
+  UdpClientHelper clientDd (StaInterfaceD.GetAddress (0), port);
+  clientDd.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
+  clientDd.SetAttribute ("Interval", TimeValue (Time ("0.00002"))); //packets/s
+  clientDd.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  ApplicationContainer clientAppDd = clientDd.Install (wifiApNodes.Get (7));
+  clientAppDd.Start (Seconds (1.0));
+  clientAppDd.Stop (Seconds (simulationTime - 1));
+
 
  //Metodo Animation
 
@@ -269,9 +320,9 @@ int main (int argc, char *argv[])
       for (uint32_t i = 0; i < wifiApNodes.GetN(); ++i)
       {
         anim.UpdateNodeDescription (wifiApNodes.Get(i), "AP"); // Optional
-        anim.UpdateNodeColor (wifiApNodes.Get(i), 255, 0, 0); // Coloração
+        anim.UpdateNodeColor (wifiApNodes.Get(i), 255, 255, 0); // Coloração
       }
-  Simulator::Stop (Seconds (simulationTime + 1));
+  Simulator::Stop (Seconds (simulationTime));
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;
