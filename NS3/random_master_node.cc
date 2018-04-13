8199366241ea063dbd4560f2266f3ddeb77c1076
Carlos_Lopes <carloslopesufpa@gmail.com>
2018-04-13 17:28:07 -0300

    New Flowmonitor
    Error Sigout IPV4

--- NS3/random_master_node.cc
+++ NS3/random_master_node.cc
@@ -52,21 +52,24 @@ double** create(int rows, int columns){
     }
     return table;
 }
-NS_LOG_COMPONENT_DEFINE ("Wifi_1");
+
+NS_LOG_COMPONENT_DEFINE ("Wifi_Random");
 
 void avalParam(int nAll, double** Vazao, double** Atraso, double** Loss, double** Energia, double** Jitterav);
 
-int cenario = 1;
-
+int cenario = 3;
+std::string gp = std::to_string(cenario);
 int main (int argc, char *argv[]) {
 
 // Simulação 1: Reconhecimento da rede.
 //Configurações da rede
-    int nRn = 4;
+    int nRn = cenario;
+// Setar Relay Nodes instalados
+    uint32_t vet[nRn][1] = {0, 1};
     int nAll = 10; 
     double simTime = 1200;
-    uint32_t MaxPacketSize = 1000;
-    double PacketInterval = 0.15;
+    uint32_t MaxPacketSize = 300;
+    double PacketInterval = 0.1;
     uint16_t grid = 50;
     std::string gr = std::to_string(grid);
 
@@ -75,13 +78,15 @@ int main (int argc, char *argv[]) {
     
 //Variáveis para receber dados do FlowMonitor
     int col = 1;
-    double** Vazao = create(nAll*nRn, col);
-    double** Atraso = create(nAll*nRn, col);
-    double** Loss = create(nAll*nRn, col);
-    double** Jitter = create(nAll*nRn, col);
+    double** Vazao = create(nAll, col);
+    double** Atraso = create(nAll, col);
+    double** Loss = create(nAll, col);
+    // double** install = create(nAll, col);
+    std::string ins[nAll][col];
+    double** Jitter = create(nAll, col);
     double atraso1 = 0;
     double atraso2 = 0;
-    double** Energia = create(nAll*nRn, col);
+    double** Energia = create(nAll, col);
     int aux_energy = 0;
       
 //Criando Nós da Simulação
@@ -116,7 +121,7 @@ if(random == true){
   std::string phyMode ("ErpOfdmRate54Mbps");
   bool verbose = false;
 
-// The below set of helpers will help us to put together the wifi NICs we want
+
   WifiHelper wifi;
   if (verbose)
     {
@@ -130,14 +135,18 @@ if(random == true){
   wifiPhy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
 
 // Criação do modelo de propagação da rede
-  YansWifiChannelHelper wifiChannel;
-  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
-  wifiChannel.AddPropagationLoss ("ns3::LogDistancePropagationLossModel",
-                                  "Exponent", DoubleValue (3.0));
+  // YansWifiChannelHelper wifiChannel;
+  // wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  // wifiChannel.AddPropagationLoss ("ns3::LogDistancePropagationLossModel",
+  //                                 "Exponent", DoubleValue (3.0));
   // YansWifiChannelHelper wifiChannel;
   // wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
   // wifiChannel.AddPropagationLoss ("ns3::TwoRayGroundPropagationLossModel",
   //                                 "Frequency", DoubleValue (5.150e9));
+
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
 
   wifiPhy.SetChannel (wifiChannel.Create ());
 
@@ -160,7 +169,7 @@ NetDeviceContainer allDevice;
 //Atribuindo energia nos dispositivos
       srand((unsigned)time(0));
       for (int l=0; l<nAll; ++l){
-          aux_energy = rand() % 100 + 50; 
+          aux_energy = rand() % 100; 
 
           Ptr<BasicEnergySource> energySource = CreateObject<BasicEnergySource>();
           Ptr<SimpleDeviceEnergyModel> energyModel = CreateObject<SimpleDeviceEnergyModel>();
@@ -191,12 +200,9 @@ NetDeviceContainer allDevice;
   uint32_t rn = 0;
   std::string ipAp[nRn][1];
   uint32_t Rn[nRn][1];
+  int x = 0;
   ApplicationContainer apps;
-  
-// ERRORRRR
-
-  int vet[nRn-1][1] = {2, 3, 6};
-    
+// Instalação dos Relay Nodes (RN)
   for(int s = 0; s < nRn; ++s){
     UdpServerHelper server (port);
     
@@ -205,7 +211,14 @@ NetDeviceContainer allDevice;
       Rn[s][0] = vet[s][0];
       rn = vet[s][0];
       }else{
-        rn = rand() % nAll;  
+// Instalação do Relay Node (RN) Aleatóriamente
+          rn = rand() % nAll; 
+          while(rn == vet[x][0]){
+           std::cout << "Antigo Rn " << rn << "\n";
+           rn = rand() % nAll;  
+           std::cout << "Novo Rn " << rn << "\n";
+           x++;
+          }
         Rn[s][0] = rn;
         server.Install (wifiAll.Get(rn));  
     }
@@ -220,7 +233,7 @@ NetDeviceContainer allDevice;
     client.SetAttribute ("MaxPackets", UintegerValue ((uint32_t)(simTime*(1/PacketInterval))));
     client.SetAttribute ("Interval", TimeValue (Seconds (PacketInterval)));
     client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
-  // Intalar Aplicação em todos os nós(Usuários)
+  // Instalar Aplicação em todos os nós Usuários
     apps = client.Install (wifiAll);
     apps.Start (Seconds (0.1));
     apps.Stop (Seconds (simTime));
@@ -232,7 +245,7 @@ NetDeviceContainer allDevice;
   Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
   
 //Gerar animação
-        AnimationInterface anim ("random_master_node.xml"); // Mandatory
+        AnimationInterface anim ("random/" + gp + "_random_master_node.xml"); // Mandatory
         
         for (uint32_t i = 0; i < wifiAll.GetN(); ++i){
           anim.UpdateNodeDescription (wifiAll.Get(i), "Node"); // Optional
@@ -240,95 +253,152 @@ NetDeviceContainer allDevice;
         }
         
         for (int i = 0; i<nRn; ++i){
-            anim.UpdateNodeDescription (wifiAll.Get(Rn[i][0]), "Server"); // Optional
+            anim.UpdateNodeDescription (wifiAll.Get(Rn[i][0]), "RN"); // Optional
             anim.UpdateNodeColor (wifiAll.Get(Rn[i][0]), 255, 255, 255); // Coloração
         }
-         
+        
         anim.EnablePacketMetadata (); // Optiona
-        anim.EnableIpv4RouteTracking ("routingtable-wireless.xml", Seconds (0), Seconds (5), Seconds (0.25)); //Optional
-        anim.EnableWifiMacCounters (Seconds (0), Seconds (10)); //Optional
-        anim.EnableWifiPhyCounters (Seconds (0), Seconds (10)); //Optional
-
+        anim.EnableIpv4RouteTracking ("random/" + gp + "_routingtable-wireless.xml", Seconds (0), Seconds (5), Seconds (0.25)); //Optional
+        anim.EnableWifiMacCounters (Seconds (0), Seconds (simTime)); //Optional
+        anim.EnableWifiPhyCounters (Seconds (0), Seconds (simTime)); //Optional
   Simulator::Stop(Seconds(simTime));
   Simulator::Run();
+ 
 
 //Imprimir resultados da simulação
   monitor->CheckForLostPackets ();
   Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier ());
   FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats ();
 
-int u = -1;
+int u = 0;
 
 double sumThroughput = 0;
 double sumLoss = 0;
-double sumEnergy = 0;
+// double sumPdr = 0;
+// double sumEnergy = 0;
 double sumAtraso = 0;
 double sumJitter = 0;
 
 double medThroughput = 0;
 double medLoss = 0;
-double medEnergy = 0;
+// double medPdr = 0;
+// double medEnergy = 0;
 double medAtraso = 0;
 double medJitter = 0;
-
+double dur = 0;
+bool install = false;
+std::string ipv;
+int l = 0;
+   
   for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin (); i != stats.end (); ++i){
-     Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
-     for(int s = 0; s<nRn; ++s){              
-        if (t.destinationAddress == ipAp[s][0].c_str()){
-            if(t.destinationAddress != t.sourceAddress){
-                u++;
-                std::cout << "Flow " << i->first << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
-                std::cout<<"Duration  : "<<(i->second.timeLastRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds())<<std::endl;
+      
+      Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+      dur = (i->second.timeLastRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds());
 
-                std::cout<<"Tx Packets = " << i->second.txPackets<<std::endl;
-                std::cout<<"Rx Packets = " << i->second.rxPackets<<std::endl;
-
-                Loss[u][0] = i->second.txPackets - i->second.rxPackets;
-                std::cout << "Perda de Pacotes: "<< Loss[u][0]<<std::endl;
+      if(dur > 0)
+      {
 
 
-                sumLoss = sumLoss + Loss[u][0];
 
-                Vazao[u][0] = i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds())/1024/1024;
-                std::cout << "Vazão: " << Vazao[u][0] << " Mbps\n";
+        for(int w = 0; w<nAll; w++)
+        {
 
-                sumThroughput = sumThroughput + Vazao[u][0];
 
-                std::cout << "Energia: "<< Energia[u][0] <<std::endl;
+           if(ins[w][0].c_str() == t.sourceAddress)
+           {
+             install = true;
+           }
+           else
+           {
+                install = false;
+           }
 
-                sumEnergy = sumEnergy + Energia[u][0];
 
-                Atraso[u][0] = ((i->second.timeLastRxPacket.GetSeconds()) - (i->second.timeLastTxPacket.GetSeconds()));
-                std::cout << "Atraso: "<< Atraso[u][0] <<std::endl;
+        } 
 
-                sumAtraso = sumAtraso + Atraso[u][0];
 
-                atraso2 = i->second.timeLastRxPacket.GetSeconds()-i->second.timeLastTxPacket.GetSeconds();
-                atraso1 = i->second.timeFirstRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds();
-                Jitter[u][0] = atraso2 - atraso1;
-                std::cout << "Jitter: "<< Jitter[u][0] <<std::endl;
 
-                sumJitter = sumJitter + Jitter[u][0];
-                
-                std::cout << " " <<std::endl;
+        if(install == false)
+        {
+            for(int s = 0; s<nRn; ++s)
+            { 
+               if (t.destinationAddress == ipAp[s][0].c_str())
+               {
+                    for(int x = 0; x<nAll; ++x)
+                    {
+                      if(t.sourceAddress == "192.168.1." + x+1)
+                      {
+                      ipv = "192.168.1." + std::to_string(x);
+                      std::cout<<"IPV4: "<< ipv <<std::endl;  
+                      
+                      }                   
+                    }
+
+                    ins[l][0] = ipv; 
+                    
+                    std::cout << "Flow " << i->first << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+                    std::cout<<"Duration  : "<< dur <<std::endl;
+                    std::cout<<"Tx Packets = " << i->second.txPackets<<std::endl;
+                    std::cout<<"Rx Packets = " << i->second.rxPackets<<std::endl;
+                    
+                    Loss[u][0] = i->second.txPackets - i->second.rxPackets;
+                    std::cout << "Perda de Pacotes: "<< Loss[u][0]<<std::endl;
+
+                    Vazao[u][0] = i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds())/1024;
+                    std::cout << "Vazão: " << Vazao[u][0] << " Kbps\n";
+
+                    std::cout << "Energia: "<< Energia[u][0] <<std::endl;
+
+                    Atraso[u][0] = ((i->second.timeLastRxPacket.GetSeconds()) - (i->second.timeLastTxPacket.GetSeconds()));
+                    std::cout << "Atraso: "<< Atraso[u][0] <<std::endl;
+
+                    atraso2 = i->second.timeLastRxPacket.GetSeconds()-i->second.timeLastTxPacket.GetSeconds();
+                    atraso1 = i->second.timeFirstRxPacket.GetSeconds()-i->second.timeFirstTxPacket.GetSeconds();
+                    Jitter[u][0] = atraso2 - atraso1;
+                    std::cout << "Jitter: "<< Jitter[u][0] <<std::endl;
+                    
+                    sumLoss = sumLoss + Loss[u][0];
+                    sumThroughput = sumThroughput + Vazao[u][0];
+                    // sumEnergy = sumEnergy + Energia[u][0];
+                    sumAtraso = sumAtraso + Atraso[u][0];
+                    sumJitter = sumJitter + Jitter[u][0];
+                    std::cout << " " <<std::endl;
+                    u++;
+                }
             }
-          } 
+            l++;
+            }else{
+            std::cout << "Flow " << i->first << " (" << t.sourceAddress << ") Já conectado\n";
+            }
       }
   }
+      
+  
+  
 // Obter Média da Simulação
    medLoss = sumLoss / (u+1);
    medThroughput = sumThroughput / (u+1);
-   medEnergy = sumEnergy / (u+1);
+   // medEnergy = sumEnergy / (u+1);
    medAtraso = sumAtraso / (u+1);
    medJitter = sumJitter / (u+1);
 
+   std::cout << " " <<std::endl;
+   std::cout << "Média Loss: "<<std::endl;
+   std::cout << "Média Vazão: "<<std::endl;
+   std::cout << "Média Atraso: "<<std::endl;
+   std::cout << "Média Jitter: "<<std::endl;
+   
+
   std::cout << " " <<std::endl;
-  std::cout << "Média Loss: "<< medLoss <<std::endl;
-  std::cout << "Média Vazão: "<< medThroughput <<std::endl;
-  std::cout << "Média Energia: "<< medEnergy <<std::endl;
-  std::cout << "Média Atraso: "<< medAtraso <<std::endl;
-  std::cout << "Média Jitter: "<< medJitter <<std::endl;
+  std::cout << medLoss <<std::endl;
+  std::cout << medThroughput <<std::endl;
+  // std::cout << medEnergy <<std::endl;
+  std::cout << medAtraso*(-1) <<std::endl;
+  std::cout << medJitter*(-1) <<std::endl;
   std::cout << " " <<std::endl;
+  std::cout << "Usuários Não Cobertos: "<< (nAll-u) <<std::endl;
+  std::cout << " " <<std::endl;
+  std::cout << "Nó Selecionado "<< rn <<std::endl;
 
 //LÓGICA DE SELEÇÃO
      
